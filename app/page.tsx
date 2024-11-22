d2a8faf150f15fa9535e39379096f114cd20a630
karurraghul <karurraghul@gmail.com>
2024-11-23 01:32:01 +0530

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
-// Change app/page.tsx to:
 'use client';
 
+import { Suspense } from 'react';
 import LandingPage from '@/components/LandingPage';
 import Home from '@/components/Home';
 import { useSearchParams } from 'next/navigation';
 
 export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <PageContent />
+    </Suspense>
+  );
+}
+
+function PageContent() {
   const searchParams = useSearchParams();
   const showApp = searchParams.get('app');
 
